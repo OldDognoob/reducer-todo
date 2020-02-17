6b762132ce61d3d93740e6e293247e900e0781ea
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-17 17:31:10 +0200

    complete file

---
+++ todo-app/src/component/TodoForm.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+
+const TodoForm = ({addTodo, clearCompleted}) =>{
+    const[item,setItem]=useState("");
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        addTodo(item);
+        setItem("");
+    };
+
+    const clearComplete = event =>{
+        event.preventDefault();
+        clearCompleted();
+    };
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input
+            type="text"
+            name="item"
+            placeholder="new todo"
+            onChange={event => setItem(event.target.name)}
+            value={item}
+            />
+            <div className="todo-btn">
+                <button className="todo-add" type="submit">
+                    Add
+                </button>
+                <button className="todo-clear" onClick={clearComplete}>
+                    Clear
+                </button>
+            </div>
+        </form>
+    );
+};
+
+export default TodoForm;
