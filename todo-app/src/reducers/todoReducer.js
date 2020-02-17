57d3f0f9ee3b1e22ac42809ef06ffccebc026671
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-17 13:53:48 +0200

    create todoReducer file and completed

---
+++ todo-app/src/reducers/todoReducer.js
@@ -0,0 +1,18 @@
+
+const initialState = {
+    todos:[
+        {
+            item: 'Learn about reducers',
+            completed: false,
+            id: 3892987589
+          }
+    ]
+};
+
+export const todoReducer(state, action){
+    switch(action.type){
+
+        default: 
+        return state
+    };
+}
