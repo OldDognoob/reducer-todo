ae9b88c3f7a19b826a23cbbe418f244b01bae3b7
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-17 20:09:24 +0200

    correction

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React, {useReducer} from 'react';
+// import TodoList from './component/TodoList';
+// import TodoForm from './component/TodoForm';
+
+// import './App.css';
+
+// import {initialState, todoReducer} from './reducers/todoReducers';
+
+// function App() {
+//   //set up the reducer state
+//   const[state,dispatch] =useReducer(todoReducer, initialState)
+
+//   const toggle = id => dispatch({ type: "TOGGLE_TODO", payload: id });
+
+
+//   const addTodo = item => dispatch({ type: "ADD_TODO", payload: item });
+
+
+//   const markCompleted = id=> {
+//     dispatch({ type:"MARK_COMPLETED", payload: id });
+//   }
+  
+//   const clearCompleted = event => {
+//     event.preventDefault();
+//     dispatch({ type: "CLEAR_COMPLETED" });
+//   } 
+  
+
+
+ 
+  
+//   return (
+//     <div className="todo">
+//       <div className ="todo-body">
+//       </div>
+//       <TodoForm addTodo ={addTodo} clearCompleted={clearCompleted}/>
+//       <TodoList todos={state.todos} toggle={toggle}/>
+      
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { useReducer } from 'react';
 import './App.css';
 
+import { initialState, todoReducer } from "./reducers/todoReducer";
+
+import "./App.css";
+
+import TodoList from './component/TodoList';
+import TodoForm from './component/TodoForm';
+
 function App() {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+
+  const addTodo = todo => {
+    dispatch({ type: 'ADD_TODO', payload: todo });
+  };
+
+  const toggleTodo = id => {
+    dispatch({ type: 'TOGGLE_TODO', payload: id });
+  };
+
+  const clearCompleted = id => {
+    dispatch({ type: 'CLEAR_COMPLETED', payload: id });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoForm addTodo={addTodo} clearCompleted={clearCompleted} />
+      <TodoList todos={state.todos} toggle={toggleTodo} />
     </div>
   );
 }
